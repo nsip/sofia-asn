336e2e734b1aa704ad845be0ccb9073fa940e1e2
Qing Miao <cdutwhu@outlook.com>
2021-10-28 23:52:04 +0000

    * add getPathByFieldValuePos with path-once audit

--- asn-json/main.go
+++ asn-json/main.go
@@ -3,88 +3,104 @@ package main
 import (
 	"log"
 	"os"
-	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/digisan/gotk"
-	"github.com/digisan/gotk/filedir"
-	jt "github.com/digisan/json-tool"
-	"github.com/nsip/sofia-asn/tool"
 )
+
+var mEscStr = map[string]string{
+	`\n`: "*LF*",
+	`\"`: "*DQ*",
+}
+
+func removeEsc(js string) string {
+	for esc, str := range mEscStr {
+		js = strings.ReplaceAll(js, esc, str)
+	}
+	return js
+}
+
+func restoreEsc(js string) string {
+	for esc, str := range mEscStr {
+		js = strings.ReplaceAll(js, str, esc)
+	}
+	return js
+}
 
 func main() {
 	defer gotk.TrackTime(time.Now())
 
-	{
-		outdir := "./out/"
-		outfile := "asn-node.json"
-		os.MkdirAll(outdir, os.ModePerm)
-		outpath := filepath.Join(outdir, outfile)
+	// {
+	// 	outdir := "./out/"
+	// 	outfile := "asn-node.json"
+	// 	os.MkdirAll(outdir, os.ModePerm)
+	// 	outpath := filepath.Join(outdir, outfile)
 
-		if !filedir.FileExists(outpath) {
-			data, err := os.ReadFile("../partition/out/node-meta.json")
-			if err != nil {
-				panic(err)
-			}
-			nodeProc(data, outdir, outfile, "../data/tree.pretty.json", "http://rdf.curriculum.edu.au/202110/")
-		}
+	// 	if !filedir.FileExists(outpath) {
+	// 		data, err := os.ReadFile("../partition/out/node-meta.json")
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		nodeProc(data, outdir, outfile, "../data/tree.pretty.json", "http://rdf.curriculum.edu.au/202110/")
+	// 	}
 
-		/////
+	// 	/////
 
-		data, err := os.ReadFile(outpath)
-		if err != nil {
-			log.Fatalln(err)
-		}
+	// 	data, err := os.ReadFile(outpath)
+	// 	if err != nil {
+	// 		log.Fatalln(err)
+	// 	}
 
-		mIdBlock, _ := getIdBlock(string(data))
+	// 	mIdBlock, _ := getIdBlock(string(data))
 
-		inpath4exp := outpath
-		outexp := childrenRepl(inpath4exp, mIdBlock)
-		// os.WriteFile("./out/asnexp.json", []byte(outexp), os.ModePerm)
+	// 	inpath4exp := outpath
+	// 	outexp := childrenRepl(inpath4exp, mIdBlock)
+	// 	// os.WriteFile("./out/asnexp.json", []byte(outexp), os.ModePerm)
 
-		rootWholeBlock := getRootWholeObject(outexp)
-		os.WriteFile("./out/asn-node-one.json", []byte(rootWholeBlock), os.ModePerm)
-	}
+	// 	rootWholeBlock := getRootWholeObject(outexp)
+	// 	os.WriteFile("./out/asn-node-one.json", []byte(rootWholeBlock), os.ModePerm)
+	// }
 
 	//////////////////////////////////////////////////////////////////////
 
 	{
 		mInputLa := map[string]string{
-			"la-English.json":                                "English",
-			"la-HASS.json":                                   "Humanities and Social Sciences",
-			"la-HPE.json":                                    "Health and Physical Education",
-			"la-Languages.json":                              "Languages",
-			"la-Mathematics.json":                            "Mathematics",
-			"la-Science.json":                                "Science",
-			"la-Technologies.json":                           "Technologies",
-			"la-The Arts.json":                               "The Arts",
-			"ccp-Cross-curriculum Priorities.json":           "",
-			"gc-Critical and Creative Thinking.json":         "",
-			"gc-Digital Literacy.json":                       "",
-			"gc-Ethical Understanding.json":                  "",
-			"gc-Intercultural Understanding.json":            "",
-			"gc-National Literacy Learning Progression.json": "",
-			"gc-National Numeracy Learning Progression.json": "",
-			"gc-Personal and Social Capability.json":         "",
+			"la-English.json": "English",
+			// "la-HASS.json":                                   "Humanities and Social Sciences",
+			// "la-HPE.json":                                    "Health and Physical Education",
+			// "la-Languages.json":                              "Languages",
+			// "la-Mathematics.json":                            "Mathematics",
+			// "la-Science.json":                                "Science",
+			// "la-Technologies.json":                           "Technologies",
+			// "la-The Arts.json":                               "The Arts",
+			// "ccp-Cross-curriculum Priorities.json":           "",
+			// "gc-Critical and Creative Thinking.json":         "",
+			// "gc-Digital Literacy.json":                       "",
+			// "gc-Ethical Understanding.json":                  "",
+			// "gc-Intercultural Understanding.json":            "",
+			// "gc-National Literacy Learning Progression.json": "",
+			// "gc-National Numeracy Learning Progression.json": "",
+			// "gc-Personal and Social Capability.json":         "",
 		}
 
-		dataTree, err := os.ReadFile("../data/tree.pretty.json")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		mCodeParent := tool.GetCodeParentMap(dataTree)
+		// dataTree, err := os.ReadFile("../data/tree.pretty.json")
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
+		// mCodeParent := tool.GetCodeParentMap(dataTree)
 
-		dataNode, err := os.ReadFile("../partition/out/node-meta.json")
-		if err != nil {
-			log.Fatalln(err)
-		}
-		mUidTitle := scanNodeIdTitle(dataNode) // title should be node title
+		// dataNode, err := os.ReadFile("../partition/out/node-meta.json")
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
+		// mUidTitle := scanNodeIdTitle(dataNode) // title should be node title
 
-		mNodeData, err := jt.Flatten(dataNode)
-		if err != nil {
-			log.Fatalln(err)
-		}
+		// mNodeData, err := jt.Flatten(dataNode)
+		// if err != nil {
+		// 	log.Fatalln(err)
+		// }
 
 		os.MkdirAll("./out", os.ModePerm)
 
@@ -98,7 +114,13 @@ func main() {
 				if err != nil {
 					log.Fatalln(err)
 				}
-				out := treeProc2(data, "http://rdf.curriculum.edu.au/202110", la, mUidTitle, mCodeParent, mNodeData)
+
+				js := removeEsc(string(data))
+
+				// out := treeProc2(data, "http://rdf.curriculum.edu.au/202110", la, mUidTitle, mCodeParent, mNodeData)
+				out := treeProc3([]byte(js))
+				
+				out = restoreEsc(out)
 				os.WriteFile("./out/"+file, []byte(out), os.ModePerm)
 				wg.Done()
 
--- asn-json/tree-process3.go
+++ asn-json/tree-process3.go
@@ -6,23 +6,39 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/digisan/gotk/slice/ts"
+	strs "github.com/digisan/gotk/strings"
 	jt "github.com/digisan/json-tool"
+	"github.com/tidwall/gjson"
+)
+
+var (
+	mStat = make(map[string]int)
 )
 
 var (
 	mRES = map[string]string{
-		"uuid":       `"uuid":\s*"[\d\w]{8}-[\d\w]{4}-[\d\w]{4}-[\d\w]{4}-[\d\w]{12}",?`,
-		"type":       `"type":\s*"\w+",?`,
-		"created_at": `"created_at":\s*"[^"]+",?`,
-		"title":      `"title":\s*".+",?`,
+		"uuid":         `"uuid":\s*"[\d\w]{8}-[\d\w]{4}-[\d\w]{4}-[\d\w]{4}-[\d\w]{12}",?`,
+		"type":         `"type":\s*"\w+",?`,
+		"created_at":   `"created_at":\s*"[^"]+",?`,
+		"title":        `"title":\s*".+",?`,
+		"doc.typeName": `"doc":\s*\{[^{}]+\},?`,
+		"code":         `"code":\s*"[^"]+",?`,
+		"text":         `"text":\s*"[^"]+",?`,
 	}
 
 	mRE4Path = map[string]*regexp.Regexp{
-		"uuid":       regexp.MustCompile(`\.?uuid$`),
-		"type":       regexp.MustCompile(`\.?type$`),
-		"created_at": regexp.MustCompile(`\.?created_at$`),
-		"title":      regexp.MustCompile(`\.?title$`),
+		"uuid":         regexp.MustCompile(`\.?uuid$`),
+		"type":         regexp.MustCompile(`\.?type$`),
+		"created_at":   regexp.MustCompile(`\.?created_at$`),
+		"title":        regexp.MustCompile(`\.?title$`),
+		"doc.typeName": regexp.MustCompile(`\.?doc\.typeName$`),
+		"code":         regexp.MustCompile(`\.?code$`),
+		"text":         regexp.MustCompile(`\.?text$`),
 	}
+
+	mRELocGrp = map[string][]int{}
+	mRELocIdx = map[string]int{}
 
 	reMerged = func() (*regexp.Regexp, map[string]*regexp.Regexp) {
 		mRE4Each := map[string]*regexp.Regexp{}
@@ -44,6 +60,89 @@ var (
 	}
 )
 
+func getPathByFieldValuePos(js, name, value string, pos int, mData map[string]interface{}) string {
+	for p, v := range mData {
+		if value == v && mRE4Path[name].MatchString(p) {
+			rst := gjson.Get(js, jt.ParentPath(p)).String()
+			if candidate := strings.Index(js, rst); candidate != -1 {
+				if candidate-(pos+len(name)+3) < 3 {
+					delete(mData, p)
+					return p
+				}
+			}
+		}
+	}
+	log.Fatalln(name, value, pos)
+	return ""
+}
+
+func proc(js, s, name, value string, mData, mData4Yr map[string]interface{}) (bool, string) {
+
+	var (
+		fSf    = fmt.Sprintf
+		uri4id = "http://rdf.curriculum.edu.au/202110"
+	)
+
+	if mRELocGrp[s] == nil {
+		mRELocGrp[s], _ = strs.IndexAll(js, s)
+	}
+
+	pos := mRELocGrp[s][mRELocIdx[s]]
+	mRELocIdx[s]++
+
+	//
+	// get 'path' once, mData removes returned path record
+	//
+	// path := getPathByFieldValuePos(js, name, value, pos, mData)
+	// fmt.Println(path)
+
+	switch name {
+	case "uuid":
+		return true, fSf(`"Id": "%s/%s"`, uri4id, value)
+
+	case "type":
+		return true, ""
+
+	case "created_at":
+		return true, fSf(`"dcterms_modified": { "literal": "%s" }`, value)
+
+	case "title":
+		return true, fSf(`"dcterms_title": { "language": "%s", "literal": "%s" }`, "en-au", value)
+
+	case "doc.typeName":
+
+		// return true, fSf(`"asn_statementLabel": { "language": "%s", "literal": "%s" }`, "en-au", value)
+
+		// ret1 := fSf(`"asn_statementLabel": { "language": "%s", "literal": "%s" }`, "en-au", value)
+		// ret2 := ""
+		if ts.NotIn(value, "Learning Area", "Subject") {
+			path := getPathByFieldValuePos(js, name, value, pos, mData)
+			fmt.Println(path)
+
+			// audit, make sure one path only for once use only
+			mStat[path]++
+
+			for _, y := range getYears(mData4Yr, path) {
+				fmt.Println(y)
+			}
+		}
+
+		// TEST
+		return true, fSf(`"asn_statementLabel": { "language": "%s", "literal": "%s" }`, "en-au", value)
+
+		// return true, strings.Join([]string{ret1, ret2}, ",")
+
+	case "code":
+		return true, fSf(`"asn_statementNotation": { "language": "%s", "literal": "%s" }`, "en-au", value)
+
+	case "text":
+		return true, fSf(`"text": "%s"`, value)
+
+	default:
+		return false, ""
+	}
+}
+
 func treeProc3(data []byte) string {
 
 	js := string(data)
@@ -53,44 +152,42 @@ func treeProc3(data []byte) string {
 		log.Fatalln(err)
 	}
 
-	proc := func(name, value string) (bool, string) {
-		// fmt.Println(name, value)
-
-		if name == "uuid" && value == "f0eaeb1b-9b65-4756-a309-9f79acf48ebd" {
-			paths := []string{}
-			for p, v := range mData {
-				if value == v && mRE4Path[name].MatchString(p) {
-					paths = append(paths, p)
-				}
-			}
-			fmt.Println(name, value, paths)
-			return true, `"uuid": ` + `"f0eaeb1b-9b65-4756-a309-9f79acf48ABC"`
-		}
-
-		if name == "type" && value == "r" {
-			return true, ""
-		}
-
-		return false, ""
+	mData4Yr := make(map[string]interface{})
+	for k, v := range mData {
+		mData4Yr[k] = v
 	}
 
 	re4json, mRE4Each := reMerged()
-	return re4json.ReplaceAllStringFunc(js, func(s string) string {
+	js = re4json.ReplaceAllStringFunc(js, func(s string) string {
 		hasComma := false
 		if strings.HasSuffix(s, ",") {
 			hasComma = true
 		}
 		for name, v := range mRE4Each {
 			if v.MatchString(s) {
-				if ok, repl := proc(name, fetchValue(s)); ok {
+
+				// if name == "doc.typeName" {
+				// 	fmt.Println(name, s)
+				// }
+
+				if ok, repl := proc(js, s, name, fetchValue(s), mData, mData4Yr); ok {
 					if hasComma && repl != "" {
 						return repl + ","
 					}
 					return repl
 				}
-				break
 			}
 		}
 		return s
 	})
+
+	// audit, make sure one path only for once use only
+	for k, v := range mStat {
+		if v > 1 {
+			log.Fatalln(k, v)
+		}
+	}
+	fmt.Println(len(mStat), "paths")
+
+	return js
 }
