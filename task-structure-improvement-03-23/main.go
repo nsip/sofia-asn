ee3d744a1bade4b4e14c7739e03fc08e8a07785f
Qing Miao <cdutwhu@outlook.com>
2023-03-21 15:53:54 +1100

    * fix 'hasLevel' un-unified style issue

--- task-structure-improvement-03-23/main.go
+++ task-structure-improvement-03-23/main.go
@@ -8,18 +8,20 @@ import (
 	"strings"
 
 	. "github.com/digisan/go-generics/v2"
+	dt "github.com/digisan/gotk/data-type"
 	fd "github.com/digisan/gotk/file-dir"
 	"github.com/digisan/gotk/strs"
+	jt "github.com/digisan/json-tool"
 )
 
 func main() {
 
 	var (
-		todo = [5]bool{true, true, true, true, true}
+		todo = [6]bool{true, true, true, true, true, true}
 	)
 
-	inDir := "../out/json-ld/MRAC/2022/06/GC/CCP"  //"../out/url-ld/"
-	outDir := "./out/json-ld/GC/CCP" // "./out/url-ld/"
+	inDir := "../out/json-ld/MRAC/2022/06/GC/CCP" // "../out/url-ld"
+	outDir := "./out/json-ld-ccp"
 
 	fd.MustCreateDir(outDir)
 
@@ -32,56 +34,79 @@ func main() {
 	for _, file := range files {
 
 		fName := file.Name()
+		fPath := filepath.Join(inDir, fName)
+
+		data, err := os.ReadFile(fPath)
+		if err != nil {
+			log.Panicln(err)
+			return
+		}
+
+		if !dt.IsJSON(data) {
+			fmt.Println("1 --->", fPath)
+			return
+		}
+
+		///////////
+
+		met := false
+		head := ""
 
 		fd.FileLineScanEx(
-			filepath.Join(inDir, fName),
+			fPath,
 			3, 3, "", func(line string, cache []string) (bool, string) {
 
 				// *** 1. date-time format ***
 				//
-				if todo[0] && strings.Contains(line, `"@value"`) {
-					ln := strings.TrimSpace(line)
-					above1, _, _ := strings.TrimSpace(cache[2]), strings.TrimSpace(cache[1]), strings.TrimSpace(cache[0])
-					below1, _, _ := strings.TrimSpace(cache[4]), strings.TrimSpace(cache[5]), strings.TrimSpace(cache[6])
-					if In(`"@type": "xsd:dateTime"`, above1, below1) {
-						val := strings.TrimPrefix(ln, `"@value": `)
-						val = strings.TrimSuffix(val, `,`)
-						val = strings.Trim(val, `"`)
-						dt, ok := TryToDateTime(val)
-						if !ok {
-							log.Println("cannot be date-time")
-							os.Exit(-1)
+				if todo[0] {
+					if strings.Contains(line, `"@value"`) {
+						ln := strings.TrimSpace(line)
+						above1, _, _ := strings.TrimSpace(cache[2]), strings.TrimSpace(cache[1]), strings.TrimSpace(cache[0])
+						below1, _, _ := strings.TrimSpace(cache[4]), strings.TrimSpace(cache[5]), strings.TrimSpace(cache[6])
+						if In(`"@type": "xsd:dateTime"`, above1, below1) {
+							val := strings.TrimPrefix(ln, `"@value": `)
+							val = strings.TrimSuffix(val, `,`)
+							val = strings.Trim(val, `"`)
+							dt, ok := TryToDateTime(val)
+							if !ok {
+								log.Println("cannot be date-time")
+								os.Exit(-1)
+							}
+							return true, fmt.Sprintf(`%s"@value": "%s",`, strs.TrimTailFromLast(line, `"@value"`), dt.Format(`2006-01-02T15:04:05.000Z`))
 						}
-						return true, fmt.Sprintf(`%s"@value": "%s",`, strs.TrimTailFromLast(line, `"@value"`), dt.Format(`2006-01-02T15:04:05.000Z`))
 					}
 				}
 
 				// *** 2. prefLabel on curriculum statements as concepts ***
 				//
-				if todo[1] && strings.Contains(line, `"asn:statementNotation"`) {
-					ln := strings.TrimSpace(line)
-					// above1, _, _ := strings.TrimSpace(cache[2]), strings.TrimSpace(cache[1]), strings.TrimSpace(cache[0])
-					// below1, _, _ := strings.TrimSpace(cache[4]), strings.TrimSpace(cache[5]), strings.TrimSpace(cache[6])
+				if todo[1] {
+					if strings.Contains(line, `"asn:statementNotation"`) {
+						ln := strings.TrimSpace(line)
+						// above1, _, _ := strings.TrimSpace(cache[2]), strings.TrimSpace(cache[1]), strings.TrimSpace(cache[0])
+						// below1, _, _ := strings.TrimSpace(cache[4]), strings.TrimSpace(cache[5]), strings.TrimSpace(cache[6])
 
-					head := strs.TrimTailFromLast(line, `"asn:statementNotation"`)
+						head := strs.TrimTailFromLast(line, `"asn:statementNotation"`)
 
-					valStr := strings.TrimPrefix(ln, `"asn:statementNotation"`)
-					valStr = strings.TrimPrefix(valStr, ":")
-					valStr = strings.TrimSpace(valStr)
-					valStr = strings.TrimSuffix(valStr, ",")
-					valStr = strings.Trim(valStr, `"`)
+						valStr := strings.TrimPrefix(ln, `"asn:statementNotation"`)
+						valStr = strings.TrimPrefix(valStr, ":")
+						valStr = strings.TrimSpace(valStr)
+						valStr = strings.TrimSuffix(valStr, ",")
+						valStr = strings.Trim(valStr, `"`)
 
-					newFieldVal := fmt.Sprintf(`"skos:prefLabel": "%s",`, valStr)
-					return true, line + "\n" + head + newFieldVal
+						newFieldVal := fmt.Sprintf(`"skos:prefLabel": "%s",`, valStr)
+						return true, line + "\n" + head + newFieldVal
+					}
 				}
 
 				// *** 3. skos shadowing of hasChild/isChildOf *** (NOT IMPLEMENT YET)
 				//
-				if todo[2] && strings.Contains(line, `"gem:hasChild"`) {
-					// skos:narrower
-				}
-				if todo[2] && strings.Contains(line, `"gem:isChildOf"`) {
-					// skos:broader
+				if todo[2] {
+					if strings.Contains(line, `"gem:hasChild"`) {
+						// skos:narrower
+					}
+					if strings.Contains(line, `"gem:isChildOf"`) {
+						// skos:broader
+					}
 				}
 
 				// *** 4. *** (No issue found as 'Remove the "connects" structure from JSON and JSON-LD output')
@@ -91,20 +116,89 @@ func main() {
 				}
 
 				// *** 5. ***
-				if todo[4] && strings.Contains(line, `"language"`) {
-					return true, strings.Replace(line, `"language"`, `"@language"`, 1)
-				}
-				if todo[4] && strings.Contains(line, `"literal"`) {
-					return true, strings.Replace(line, `"literal"`, `"@value"`, 1)
-				}
-				if todo[4] && strings.Contains(line, `"position"`) {
-					return true, strings.Replace(line, `"position"`, `"@asn:listID"`, 1)
+				if todo[4] {
+					if strings.Contains(line, `"language"`) {
+						return true, strings.Replace(line, `"language"`, `"@language"`, 1)
+					}
+					if strings.Contains(line, `"literal"`) {
+						return true, strings.Replace(line, `"literal"`, `"@value"`, 1)
+					}
+					if strings.Contains(line, `"position"`) {
+						return true, strings.Replace(line, `"position"`, `"@asn:listID"`, 1)
+					}
 				}
 				//
+
+				// *** 6. unify asn:hasLevel style ***
+				if todo[5] {
+
+					// above1, _, _ := strings.TrimSpace(cache[2]), strings.TrimSpace(cache[1]), strings.TrimSpace(cache[0])
+					below1, _, _ := strings.TrimSpace(cache[4]), strings.TrimSpace(cache[5]), strings.TrimSpace(cache[6])
+
+					if !met && strings.TrimSpace(line) == `"asn:hasLevel": {` {
+						head = strs.TrimTailFromLast(line, `"asn:hasLevel"`)
+						met = true
+						return true, head + `"asn:hasLevel": [{`
+					}
+					if met && line == head+"}" {
+						met = false
+						return true, head + `}]`
+					}
+
+					if below1 == `"asn:hasLevel": []` {
+						return true, strings.TrimSuffix(line, ",")
+					}
+					if strings.Contains(line, `"asn:hasLevel": []`) {
+						return false, ""
+					}
+				}
 
 				return true, line
 			},
 			filepath.Join(outDir, fName),
 		)
+	}
+
+	// *** format output ***
+
+	files, err = os.ReadDir(outDir)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	for _, file := range files {
+		fName := file.Name()
+		fPath := filepath.Join(outDir, fName)
+		data, err := os.ReadFile(fPath)
+		if err != nil {
+			log.Panicln(err)
+			return
+		}
+		if err := os.WriteFile(fPath, jt.Fmt(data, "    "), os.ModePerm); err != nil {
+			fmt.Println(fPath)
+			log.Panicln(err)
+			return
+		}
+	}
+
+	// *** check valid json ***
+
+	files, err = os.ReadDir(outDir)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	for _, file := range files {
+		fName := file.Name()
+		fPath := filepath.Join(outDir, fName)
+		data, err := os.ReadFile(fPath)
+		if err != nil {
+			log.Panicln(err)
+			return
+		}
+		if !dt.IsJSON(data) {
+			fmt.Println("2 --->", fPath)
+			return
+		}
 	}
 }
