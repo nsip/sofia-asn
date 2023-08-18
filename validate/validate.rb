1bbc2f9a1b5466ec3eb812c1b801a254f3ff88e4
Nick Nicholas <opoudjis@gmail.com>
2023-08-18 20:52:16 +1000

    improve validate script

--- validate/validate.rb
+++ validate/validate.rb
@@ -43,7 +43,7 @@ end
 def asn_jsonld_id_parse(json, ids)
   if (k = id(json)) && json['asn:statementLabel'] # if no label, it's a root of tree, not a real ID
     ids[k] and @err.puts "#{@f}: id_parse: #{k} is duplicated beween #{@ids[k]} and #{@f}!"
-    ids[k] = { file: @f.sub(/^.+\//, ""), label: json['asn:statementLabel'] }
+    ids[k] = { file: @f.sub(%r{^.+/}, ''), label: json['asn:statementLabel'] }
   end
   json['gem:hasChild']&.each do |m|
     ids = asn_jsonld_id_parse(m, ids)
@@ -64,13 +64,17 @@ def asn_json_id_parse(json, ids)
   if (k = id(json))
     keys = json.keys.reject { |x| json[x].nil? }
     untitled = ['Cross-Curriculum Priority']
-    #if !json['asn_statementLabel'] || untitled.include?(json['asn_statementLabel']) ||
+    # if !json['asn_statementLabel'] || untitled.include?(json['asn_statementLabel']) ||
     #   untitled.include?(json.dig('asn_statementLabel', 'literal'))
     # More trouble than it's worth
-    keys = keys.reject { |x| %w(dcterms_title dcterms_description esa_nominalYearLevel).include?(x) }
-    #end
-    ids[k] = { file: @f.sub(/^.+\//, ""), keys: keys.map { |x| x.sub('_', ':').sub('dcterms:', 'dc:') }
-      .select { |x| x.include?(':') }.sort  }
+    if keys.include?('dcterms_description')
+      keys = keys.reject { |x| x == 'dcterms_description' }
+      keys.include?('dcterms_title') or keys << 'dcterms_title'
+    end
+    keys = keys.reject { |x| %w[esa_nominalYearLevel].include?(x) }
+    # end
+    ids[k] = { file: @f.sub(%r{^.+/}, ''), keys: keys.map { |x| x.sub('_', ':').sub('dcterms:', 'dc:') }
+                                                     .select { |x| x.include?(':') }.sort }
     ids['children'] and ids[k] += 'gem:hasChild'
   end
   json['children']&.each do |m|
@@ -92,13 +96,15 @@ def child_parent(json)
     j['gem:isChildOf']&.each do |k|
       id(json) or next
       (id(json) == id(k)) and next
-      @err.puts "#{@f}: child_parent: #{id(j)} mismatches parent and child!"
+      @err.puts "#{@f}: child_parent: #{id(j)} mismatches gem:hasChild and gem:isChildOf!"
     end
     child_parent(j)
   end
 end
 
 def id(x)
+  require 'debug'
+  x.is_a?(Hash) or binding.b
   x['@id'] || x['@Id'] || x['id'] || x['Id']
 end
 
@@ -109,7 +115,7 @@ def namespaced_keys(json, key)
       namespaced_keys(json[k], k)
       key == '@context' and next
       !/\s/.match?(k) && /\S:\S|^@/.match?(k) and next
-      @err.puts "#{@f}: namespaced_keys: #{k} is an illegal key!"
+      @err.puts "#{@f}: namespaced_keys: #{k} in #{id(json)} is an illegal key!"
     end
   when Array
     json.each { |j| namespaced_keys(j, nil) }
@@ -159,7 +165,7 @@ def link_skillembodied(json)
   json['asn:skillEmbodied'].each do |k|
     exist_link(k['@id'], 'asn:skillEmbodied') or next
     unless /^gc/.match?(@ids[k['@id']][:file])
-      @err.puts "#{@f}: link_types: #{k['@id']} destination of asn:skillEmbodied is not a general capability!"
+      @err.puts "#{@f}: link_types: #{k['@id']} destination of asn:skillEmbodied in #{json['@id']} is not a general capability!"
     end
   end
 rescue StandardError => e
@@ -172,7 +178,7 @@ def link_cross_subject_reference(json)
   json['asn:crossSubjectReference'].each do |k|
     exist_link(k['@id'], 'asn:crossSubjectReference') or next
     /^ccp|^AA|^A_TSI|^S/.match?(@ids[k['@id']][:file]) or
-      @err.puts "#{@f}: link_types: #{k['@id']} destination of asn:crossSubjectReference is not a cross-curriculum priority!"
+      @err.puts "#{@f}: link_types: #{k['@id']} destination of asn:crossSubjectReference in #{json['@id']} is not a cross-curriculum priority!"
   end
 rescue StandardError => e
   require 'debug'
@@ -184,7 +190,7 @@ def link_has_level(json)
   json['asn:hasLevel'].each do |k|
     exist_link(k['@id'], 'asn:hasLevel') or next
     ['Level', 'Achievement Standard', 'Achievement Standard Component'].include?(@ids[k['@id']][:label]) or
-      @err.puts "#{@f}: link_types: #{k['@id']} destination of asn:hasLevel is #{@ids[k['@id']][:label]}, not an achievement level!"
+      @err.puts "#{@f}: link_types: #{k['@id']} destination of asn:hasLevel in #{json['@id']} is #{@ids[k['@id']][:label]}, not an achievement level!"
   end
 rescue StandardError => e
   require 'debug'
@@ -196,7 +202,7 @@ def link_dc_relation(json)
   json['dc:relation'].each do |k|
     exist_link(k['@id'], 'dc:relation') or next
     /^la/.match?(@ids[k['@id']][:file]) or
-      @err.puts "#{@f}: link_types: #{k['@id']} destination of dc:relation is not a curriculum statement!"
+      @err.puts "#{@f}: link_types: #{k['@id']} destination of dc:relation in #{json['@id']} is not a curriculum statement!"
   end
 rescue StandardError => e
   require 'debug'
@@ -235,8 +241,10 @@ end
 
 def consistent_nominal_yr_level(json)
   if json['dc:educationLevel'] && json['esa:nominalYearLevel']
-    unless subset?(extract_yr_levels(json['dc:educationLevel']), extract_yr_levels(json['esa:nominalYearLevel']))
-      @err.puts "#{@f}: year levels: #{json['@id']} dc:educationLevel and esa:nominalYearLevel inconsistent"
+    lvl = extract_yr_levels(json['dc:educationLevel'])
+    nomnl = extract_yr_levels(json['esa:nominalYearLevel'])
+    unless subset?(lvl, nomnl)
+      @err.puts "#{@f}: year levels: dc:educationLevel (#{lvl.join(', ')}) and esa:nominalYearLevel (#{nomnl.join(', ')})  in #{json['@id']} are inconsistent"
     end
   end
   json['gem:hasChild']&.each do |m|
@@ -259,7 +267,7 @@ def inferred_yr_level(json)
     lvl_yrs = extract_yr_levels(json['dc:educationLevel'])
     unless id_yrs.empty? || id_yrs.empty?
       subset?(id_yrs, lvl_yrs) or
-        @err.puts "#{@f}: year levels: #{json['@id']} node year levels are inconsistent with ID #{id}"
+        @err.puts "#{@f}: year levels: #{json['@id']} node year levels are inconsistent with identifier #{id} in #{json['@id']}"
     end
   end
   json['gem:hasChild']&.each do |m|
@@ -277,10 +285,8 @@ end
 
 def inherit_yr_level(json, parent_yrs = [])
   y = extract_yr_levels(json['dc:educationLevel'])
-  unless parent_yrs.empty? || y.empty?
-    unless subset?(parent_yrs, y)
-      @err.puts "#{@f}: year levels: #{id(json)} node year levels are not subset of parent node year levels"
-    end
+  if !(parent_yrs.empty? || y.empty?) && !subset?(parent_yrs, y)
+    @err.puts "#{@f}: year levels: #{id(json)} node year levels (#{y.join(', ')}) are not subset of parent node year levels (#{parent_yrs.join(', ')})"
   end
   json['gem:hasChild']&.each do |m|
     inherit_yr_level(m, y)
@@ -298,11 +304,17 @@ end
 def match_predicates_json_ld(json)
   json_keys = @asnjson.dig(id(json), :keys)
   jsonld_keys = json.keys.select { |x| x.include?(':') }
-                    .reject { |x| %w[gem:hasChild gem:isChildOf dc:title dc:description dc:isPartOf esa:nominalYearLevel].include?(x) }.sort
-  if json_keys != jsonld_keys && !jsonld_keys.include?("skos:prefLabel") # prefLabel means this is not a node, but an alias
-    require 'debug'
-    binding.b
-    @err.puts "#{@f}: JSON: #{id(json)} inconsistent predicates between JSON and JSON-LD"
+                    .reject { |x| %w[gem:hasChild gem:isChildOf dc:isPartOf esa:nominalYearLevel].include?(x) }
+  if jsonld_keys.include?('dc:description')
+    jsonld_keys = jsonld_keys.reject { |x| x == 'dc:description' }
+    jsonld_keys.include?('dc:title') or jsonld_keys << 'dc:title'
+  end
+  jsonld_keys.sort!
+  if json_keys != jsonld_keys && !jsonld_keys.include?('skos:prefLabel') # prefLabel means this is not a node, but an alias
+    l = json['asn:statementLabel']
+    l.is_a?(Hash) and l = json.dig('asn:statementLabel', 'literal')
+    keys_diff = keys_diff(json_keys, jsonld_keys)
+    @err.puts "#{@f}: JSON (#{l}): inconsistent predicates between JSON and JSON-LD (#{keys_diff}) in #{id(json)}"
   end
   json['gem:hasChild']&.each do |m|
     match_predicates_json_ld(m)
@@ -317,14 +329,25 @@ def match_predicates_json_ld(json)
   end
 end
 
+def keys_diff(arr1, arr2)
+  ret = []
+  arr1.each do |k|
+    arr2.include?(k) or ret << "-#{k}"
+  end
+  arr2.each do |k|
+    arr1.include?(k) or ret << "+#{k}"
+  end
+  ret.join(', ')
+end
+
 def extract_yr_levels(json)
   json.nil? and return []
   case json
   when Array
     json.map { |j| extract_yr_levels(j) }.flatten
   when Hash
-    [id(json).sub(%r{^.+/}, '')]
-  else [json.sub(%r{^.+/}, '')]
+    [id(json).sub(%r{^.+/}, '').sub("Foundation Year", "0").sub(/^Year\s+/, "")]
+  else [json.sub(%r{^.+/}, '').sub("Foundation Year", "0").sub(/^Year\s+/, "")]
   end
 end
 
